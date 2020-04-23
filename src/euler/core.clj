d3f6f604f65770225fb90a625cbe1ba6312a8b45
lambnet <alviannurramadhani@gmail.com>
2020-04-24 00:36:35 +0700

    problem 4 and 6

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -21,3 +21,42 @@
        (apply +)))
 
 ;; Problem 3
+;; Problem 4
+(defn reverse-num
+  [n]
+  (Integer. (apply str (reverse (str n)))))
+
+(defn eul4
+  []
+  (for [a (range 100 1001)
+        b (range a 1001)]
+    (if (= (* a b) (reverse-num (* a b))) (* a b))))
+
+(defn eul4a
+  []
+  (->> (eul4)
+       (sort)
+       (drop-while nil?)
+       (last)))
+
+;; Problem 5
+;; Problem 6
+(defn square
+  [n]
+  (* n n))
+
+(defn sum-of-squares
+  [n]
+  (/ (* (+ (* 2 n) 1)
+        (+ n 1)
+        n)
+     6))
+
+(defn square-of-sum
+  [n]
+  (square (/ (* (+ n 1) n)
+             2)))
+
+(defn eul6
+  [n]
+  (- (square-of-sum n) (sum-of-squares n)))
